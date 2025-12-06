9ca2d42803a55f79d412d984a165b78ac5ae7d9c
LucianoLupo95 <lnlupo95@gmail.com>
2025-12-06 13:08:45 +0100

    Conexión a bbdd

---
+++ db/connection.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var DB *sqlx.DB
+
+func Connect() {
+    // Load environment variables
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Println("No pude cargar el .env en la carpeta actual")
+	}
+
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    dbname := os.Getenv("DB_NAME")
+    sslmode := os.Getenv("DB_SSLMODE")
+
+    dsn := fmt.Sprintf(
+        "postgres://%s:%s@%s:%s/%s?sslmode=%s",
+        user, password, host, port, dbname, sslmode,
+    )
+
+    conn, err := sqlx.Connect("postgres", dsn)
+    if err != nil {
+        log.Fatal("Error connecting to database:", err)
+    }
+
+    fmt.Println("Database connected successfully!")
+    DB = conn
+}
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucianolupo95/wardrobe-backend/db"
+)
 
 func main() {
+    db.Connect()
     r := gin.Default()
 
     r.GET("/health", func(c *gin.Context) {
